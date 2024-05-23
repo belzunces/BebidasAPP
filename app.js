4af8b9803bbc3de17850922bd2b376f95ecae6a4
belzunces <75071366+belzunces@users.noreply.github.com>
2024-05-23 17:31:44 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 document.getElementById('pedidoForm').addEventListener('submit', function(event) {
     event.preventDefault();
     const mesa = document.getElementById('mesa').value;
-    const checkboxes = document.querySelectorAll('input[name="bebida"]:checked');
+    const inputs = document.querySelectorAll('input[name="bebida"]');
     const selectedBebidas = [];
-    checkboxes.forEach((checkbox) => {
-        selectedBebidas.push(checkbox.value);
+    inputs.forEach((input) => {
+        if (input.value > 0) {
+            selectedBebidas.push(`${input.value} ${input.name}`);
+        }
     });
+
     const result = `
         <h3>Informe del Pedido</h3>
         <p><strong>Número de Mesa:</strong> ${mesa}</p>
-        <p><strong>Bebidas Seleccionadas:</strong> ${selectedBebidas.join(', ')}</p>
-    `;
-    document.getElementById('result').innerHTML = result;
-});
+        <p><strong>Bebidas Seleccionadas:</strong> ${selectedBebidas.join(', ')
